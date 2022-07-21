8c0a2c0f9324fa2427f3d771bfa60d788bdc5c72
Tilek <zholdoshbek_t@auca.kg>
2022-07-21 20:41:59 +0600

    Deleted mail.debug=true, show-sql=true; Modified generate method

--- medtech_main/src/main/java/com/project/medtech/config/SecurityConfig.java
+++ medtech_main/src/main/java/com/project/medtech/config/SecurityConfig.java
@@ -113,7 +113,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
 
         return mailSender;
     }
