6cb7917c9d73bb5cbd165f737ae69d91ec1dbe03
TechLine <quantech7101@gmail.om>
2022-08-06 18:28:12 +0600

    adding new methods to checklistRep

---
+++ medtech_main/src/main/java/com/project/medtech/repository/DoctorScheduleRepository.java
@@ -0,0 +1,16 @@
+package com.project.medtech.repository;
+
+import com.project.medtech.model.DoctorSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Repository
+public interface DoctorScheduleRepository extends JpaRepository<DoctorSchedule, Long> {
+    @Query("SELECT  ch FROM DoctorSchedule ch WHERE ch.doctor.id =:docID AND ch.dayOfWeek =:cDate")
+    Optional<DoctorSchedule> findByDocIDAndDate(@RequestParam Long docID, Date cDate);
+}
